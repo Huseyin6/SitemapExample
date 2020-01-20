977577966dca26114e14be4b70d35d58c331eed2
Huseyin6 <hsynsimsek723@gmail.com>
2020-01-20 16:50:45 +0300

    Added Repo

---
+++ SitemapExample.Web/Controllers/SitemapController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookieUsage.Web.Controllers
+{
+    public class SitemapController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public SitemapController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        [Route("sitemap_index.xml")]
+        public void Get()
+        {
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.LoadXml(GetSitemapIndexXMLString());
+            }
+            catch (Exception)
+            {
+            }
+            var result = doc.InnerXml;
+            if (String.IsNullOrEmpty(result)) return;
+            var response = _httpContextAccessor.HttpContext.Response;
+            response.Clear();
+            response.ContentType = "application/xml";
+            response.WriteAsync(result);
+            response.CompleteAsync();
+        }
+
+        [Route("userpages.xml")]
+        public void GetCVPage()
+        {
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.LoadXml(GetCvPageXMLString());
+            }
+            catch (Exception)
+            {
+            }
+            var result = doc.InnerXml;
+            if (String.IsNullOrEmpty(result)) return;
+            var response = _httpContextAccessor.HttpContext.Response;
+            response.Clear();
+            response.ContentType = "application/xml";
+            response.WriteAsync(result);
+            response.CompleteAsync();
+        }
+    
+        private string GetCvPageXMLString()
+        {
+            using (StringWriter str = new Utf8StringWriter())
+            using (XmlWriter writer = XmlWriter.Create(str))
+            {
+                XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                ns.Add("", "http://www.sitemaps.org/schemas/sitemap/0.9");
+                XmlSerializer xser = new XmlSerializer(typeof(SiteMapModel));
+                var obj = new SiteMapModel();
+                List<string> users = new List<string> { "AliPage.com", "VeliPage.com", "AysePage.com" };// Veritabanından bir kaynaktan gelebilir.
+                foreach (var user in users)
+                {
+                    if (!String.IsNullOrEmpty(user))
+                        obj.SiteMapUrls.Add(new SiteMapNodeModel() { Url = user , LastModified=DateTime.Now.ToString(), Priority="0.8", Frequency= SiteMapFrequency.Monthly.ToString()});
+                }
+                xser.Serialize(writer, obj, ns);
+                return str.ToString();
+            }
+        }
+
+        private string GetSitemapIndexXMLString()
+        {
+            using (StringWriter str = new Utf8StringWriter())
+            using (XmlWriter writer = XmlWriter.Create(str))
+            {
+                XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                ns.Add("", "http://www.sitemaps.org/schemas/sitemap/0.9");
+                XmlSerializer xser = new XmlSerializer(typeof(SiteMapIndexModel));
+                var obj = new SiteMapIndexModel
+                {
+                    SiteMapIndexUrls = {
+                        new SiteMapIndexNodeModel { Url = "userpages.xml"},
+                        new SiteMapIndexNodeModel { Url = "testpage.xml"}
+                    }
+                };
+                xser.Serialize(writer, obj, ns);
+                return str.ToString();
+            }
+        }
+
+        private class Utf8StringWriter : StringWriter
+        {
+            public override Encoding Encoding => Encoding.UTF8;// utf16 formatını utf8 olarak ezmesi için.  
+        }
+    }
+}
---
+++ SitemapExample.Web/Models/SiteMapModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CookieUsage.Web
+{
+
+    [XmlRoot("urlset", Namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")]
+    public class SiteMapModel
+    {
+        private readonly List<SiteMapNodeModel> urls = new List<SiteMapNodeModel>();
+
+        [XmlElement("url")]
+        public List<SiteMapNodeModel> SiteMapUrls { get { return urls; } }
+    }
+
+    public class SiteMapNodeModel
+    {
+        [XmlElement("loc")]
+        public string Url { get; set; }
+
+        [XmlElement("changefreq")]
+        public string Frequency { get; set; }
+
+        [XmlElement("lastmod")]
+        public string LastModified { get; set; }
+
+        [XmlElement("priority")]
+        public string Priority { get; set; }
+    }
+
+    [XmlRoot("sitemapindex", Namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")]
+    public class SiteMapIndexModel
+    {
+        private readonly List<SiteMapIndexNodeModel> urls = new List<SiteMapIndexNodeModel>();
+
+        [XmlElement("sitemap")]
+        public List<SiteMapIndexNodeModel> SiteMapIndexUrls { get { return urls; } }
+    }
+
+    public class SiteMapIndexNodeModel
+    {
+        [XmlElement("loc")]
+        public string Url { get; set; }
+
+        [XmlElement("lastmod")]
+        public string LastModified { get; set; }
+    }
+
+    public enum SiteMapFrequency
+    {
+        Never,
+        Yearly,
+        Monthly,
+        Weekly,
+        Daily,
+        Hourly,
+        Always
+    }
+
+}
